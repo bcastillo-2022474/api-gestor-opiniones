5166d24bf9de630967fdc4b85776d88e489b6de5
bcastillo-2022474 <bcastillo-2022474@kinal.edu.gt>
2024-03-02 09:44:23 -0600

    Creating routes, using named routes instead of crud for readability

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,25 @@
 import express from 'express';
 import User from '../models/user.model.js';
 import { loginController } from '../controller/user.controller.js';
+import { hashPassword, comparePassword } from '../helpers/bcrypt.js';
 import isLoggedIn from '../middlewares/isLoggedIn.js';
 
 const router = express.Router();
 
-router.get('/mycomments', isLoggedIn, async (req, res) => {
+router.get('/profile', isLoggedIn, async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).select(
+      '-password -posts -comments -__v'
+    );
+
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: error.message });
+  }
+});
+
+router.get('/comments', isLoggedIn, async (req, res) => {
   try {
     const user = await User.findById(req.user._id).populate({
       path: 'comments',
@@ -16,7 +30,48 @@ router.get('/mycomments', isLoggedIn, async (req, res) => {
       },
     });
 
-    res.json(user);
+    res.json(user.comments);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: error.message });
+  }
+});
+
+router.get('/posts', isLoggedIn, async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).populate({
+      path: 'posts',
+      select: '-__v',
+      populate: {
+        path: 'author',
+        select: '-_id name lastName username',
+      },
+    });
+
+    res.json(user.posts);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: error.message });
+  }
+});
+
+router.post('/register', async (req, res) => {
+  try {
+    const { name, lastName, email, username, password } = req.body;
+
+    const user = new User({
+      name,
+      lastName,
+      email,
+      username,
+      password: await hashPassword(password),
+      posts: [],
+      comments: [],
+    });
+
+    await user.save();
+
+    res.json({ message: 'User created' });
   } catch (error) {
     console.error(error);
     res.status(400).json({ message: error.message });
@@ -27,11 +82,60 @@ router.post('/login', async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
-    if (username && email) {
-      return res
-        .status(400)
-        .json({ message: 'Only one of username or email is required' });
+router.put('/update', isLoggedIn, async (req, res) => {
+  try {
+    const { name, lastName, email, username } = req.body;
+    const user = req.user;
+
+    user.name = name;
+    user.lastName = lastName;
+    user.email = email;
+    user.username = username;
+
+    let usernameAlreadyExists = await User.findOne({ username });
+    let emailAlreadyExists = await User.findOne({ email });
+
+    if (
+      usernameAlreadyExists &&
+      usernameAlreadyExists._id.toString() !== user._id.toString()
+    ) {
+      return res.status(400).json({ message: 'Username already exists' });
+    } else if (
+      emailAlreadyExists &&
+      emailAlreadyExists._id.toString() !== user._id.toString()
+    ) {
+      return res.status(400).json({ message: 'Email already exists' });
     }
+
+    await user.save();
+
+    res.json({ message: 'User updated' });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: error.message });
+  }
+});
+
+router.put('/update/password', isLoggedIn, async (req, res) => {
+  try {
+    const { oldPassword, newPassword } = req.body;
+    const user = req.user;
+
+    const isPasswordCorrect = await comparePassword(oldPassword, user.password);
+
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: 'Old password is incorrect' });
+    }
+
+    user.password = await hashPassword(newPassword);
+    await user.save();
+
+    res.json({ message: 'Password updated' });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: error.message });
+  }
+});
 
     const user = await User.findOne({ $or: [{ username }, { email }] });
 
