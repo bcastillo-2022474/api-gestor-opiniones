cd53e73215ec005120e04314270bd96cb956b6ff
bcastillo-2022474 <bcastillo-2022474@kinal.edu.gt>
2024-03-02 09:40:04 -0600

    Merge of changes

---
+++ src/routes/comment.routes.js
@@ -0,0 +1,94 @@
+import express from 'express';
+import isLoggedIn from '../middlewares/isLoggedIn.js';
+import Post from '../models/post.model.js';
+import Comment from '../models/comment.model.js';
+
+const router = express.Router();
+
+router.post('/:idPost', isLoggedIn, async (req, res) => {
+  try {
+    const { idPost } = req.params;
+    const { content } = req.body;
+    const user = req.user;
+
+    const post = await Post.findById(idPost);
+
+    if (!post) {
+      return res.status(400).json({ message: 'Post not found' });
+    }
+
+    const newComment = new Comment({
+      author: user._id,
+      content,
+    });
+
+    await newComment.save();
+
+    post.comments.push(newComment._id);
+    await post.save();
+
+    user.comments.push(newComment._id);
+    await user.save();
+
+    return res.json(post);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: error.message });
+  }
+});
+
+router.put('/:idComment', isLoggedIn, async (req, res) => {
+  try {
+    const { idComment } = req.params;
+    const { content } = req.body;
+    const user = req.user;
+
+    const comment = await Comment.findById(idComment);
+
+    if (!comment) {
+      return res.status(400).json({ message: 'Comment not found' });
+    }
+
+    if (comment.author.toString() !== user._id.toString()) {
+      return res
+        .status(400)
+        .json({ message: 'You are not the author of this comment' });
+    }
+
+    comment.content = content;
+    await comment.save();
+
+    return res.json(comment);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: error.message });
+  }
+});
+
+router.delete('/:idComment', isLoggedIn, async (req, res) => {
+  try {
+    const { idComment } = req.params;
+    const user = req.user;
+
+    const comment = await Comment.findById(idComment);
+
+    if (!comment) {
+      return res.status(400).json({ message: 'Comment not found' });
+    }
+
+    if (comment.author.toString() !== user._id.toString()) {
+      return res
+        .status(400)
+        .json({ message: 'You are not the author of this comment' });
+    }
+
+    await Comment.findByIdAndDelete(idComment);
+
+    return res.json({ message: 'Comment removed' });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: error.message });
+  }
+});
+
+export default router;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,26 @@
 import express from 'express';
-import { hashPassword, comparePassword } from '../helpers/bcrypt.js';
-import { createToken } from '../helpers/jwt.js';
 import User from '../models/user.model.js';
+import { loginController } from '../controller/user.controller.js';
+import isLoggedIn from '../middlewares/isLoggedIn.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json({ message: 'Welcome to the user routes' });
+router.get('/mycomments', isLoggedIn, async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).populate({
+      path: 'comments',
+      select: '-__v -_id',
+      populate: {
+        path: 'author',
+        select: '-_id name lastName username',
+      },
+    });
+
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: error.message });
+  }
 });
 
 router.post('/login', async (req, res) => {
@@ -43,5 +57,6 @@ router.post('/login', async (req, res) => {
     res.status(500).json({ message: 'Internal server error' });
   }
 });
+
 
 export default router;
