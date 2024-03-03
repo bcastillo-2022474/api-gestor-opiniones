8235d4814a9f5a31fce8cf6f98eb18f258bf159e
bcastillo-2022474 <bcastillo-2022474@kinal.edu.gt>
2024-03-03 02:41:15 -0600

    Final minor changes

---
+++ src/controller/category.controller.js
@@ -0,0 +1,85 @@
+import Category from '../models/category.model.js';
+import Post from '../models/post.model.js';
+
+export const getAllCategories = async (req, res) => {
+  try {
+    const categories = await Category.find({}).select('-__v');
+
+    res.json(categories);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const createCategory = async (req, res) => {
+  try {
+    const { name } = req.body;
+
+    const categories = await Category.find({});
+
+    const categoryExists = categories.some(
+      (category) => category.name === name
+    );
+
+    if (categoryExists) {
+      return res.status(400).json({ message: 'Category already exists' });
+    }
+
+    const category = new Category({
+      name,
+    });
+
+    await category.save();
+
+    res.json(category);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const deleteCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const category = await Category.findById(id);
+
+    if (!category) {
+      return res.status(400).json({ message: 'Category not found' });
+    }
+
+    const posts = await Post.find({ category: category._id })
+      .select('-__v')
+      .populate({
+        path: 'category',
+        select: '-_id name',
+      })
+      .populate({
+        path: 'author',
+        select: '-_id name lastName username',
+      })
+      .populate({
+        path: 'comments',
+        select: '-__v',
+        populate: {
+          path: 'author',
+          select: '-_id name lastName username',
+        },
+      });
+
+    if (posts.length > 0) {
+      return res.status(400).json({
+        message: 'Category has posts associated',
+        postsAssociated: posts,
+      });
+    }
+
+    await Category.findByIdAndDelete(id);
+
+    res.json({ message: 'Category deleted' });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: error.message });
+  }
+};
--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -1,91 +1,15 @@
 import express from 'express';
-import Category from '../models/category.model.js';
-import Post from '../models/post.model.js';
 import isLoggedIn from '../middlewares/isLoggedIn.js';
+import {
+  getAllCategories,
+  createCategory,
+  deleteCategory,
+} from '../controller/category.controller.js';
 
 const router = express.Router();
 
-router.get('/all', isLoggedIn, async (req, res) => {
-  try {
-    const categories = await Category.find({}).select('-__v');
-
-    res.json(categories);
-  } catch (error) {
-    console.error(error);
-    res.status(400).json({ message: error.message });
-  }
-});
-
-router.post('/create', isLoggedIn, async (req, res) => {
-  try {
-    const { name } = req.body;
-
-    const categories = await Category.find({});
-
-    const categoryExists = categories.some(
-      (category) => category.name === name
-    );
-
-    if (categoryExists) {
-      return res.status(400).json({ message: 'Category already exists' });
-    }
-
-    const category = new Category({
-      name,
-    });
-
-    await category.save();
-
-    res.json(category);
-  } catch (error) {
-    console.error(error);
-    res.status(400).json({ message: error.message });
-  }
-});
-
-router.delete('/delete/:id', isLoggedIn, async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    const category = await Category.findById(id);
-
-    if (!category) {
-      return res.status(400).json({ message: 'Category not found' });
-    }
-
-    const posts = await Post.find({ category: category._id })
-      .select('-__v')
-      .populate({
-        path: 'category',
-        select: '-_id name',
-      })
-      .populate({
-        path: 'author',
-        select: '-_id name lastName username',
-      })
-      .populate({
-        path: 'comments',
-        select: '-__v',
-        populate: {
-          path: 'author',
-          select: '-_id name lastName username',
-        },
-      });
-
-    if (posts.length > 0) {
-      return res.status(400).json({
-        message: 'Category has posts associated',
-        posts,
-      });
-    }
-
-    await Category.findByIdAndDelete(id);
-
-    res.json({ message: 'Category deleted' });
-  } catch (error) {
-    console.error(error);
-    res.status(400).json({ message: error.message });
-  }
-});
+router.get('/all', isLoggedIn, getAllCategories);
+router.post('/create', isLoggedIn, createCategory);
+router.delete('/delete/:id', isLoggedIn, deleteCategory);
 
 export default router;
